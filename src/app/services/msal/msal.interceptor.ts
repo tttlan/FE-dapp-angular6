9f856676d287e261f46073a25757cbf9483d83d2
naruto <nguyethakiemkhach@gmail.com>
2018-08-22 11:04:50 +0700

    angular 6 skeleton

--- src/app/services/msal/msal.interceptor.ts
+++ src/app/services/msal/msal.interceptor.ts
@@ -3,13 +3,14 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 
 import { MsalService } from "./msal.service";
+import { AuthService } from "../auth/auth.service";
 
 @Injectable()
 export class MsalInterceptor implements HttpInterceptor {
-    constructor(private msal: MsalService) { }
+    constructor(private msal: MsalService, private auth: AuthService) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        if (this.msal.isOnline()) {
+        if (this.msal.isOnline() && !this.auth.isAuthenticated) {
             this.msal.getToken().then(token => {
                 req = req.clone({
                     setHeaders: {
